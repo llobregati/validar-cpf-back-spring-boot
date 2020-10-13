79dd23f0665a60889703c6fd37ea47074fa09ae6
LeandroLobregati <llobregatil@gmail.com>
2020-10-13 17:24:01 -0300

    alteração de versão correção

--- src/main/java/br/com/professorisidro/validacpf/controller/ValidadorController.java
+++ src/main/java/br/com/professorisidro/validacpf/controller/ValidadorController.java
@@ -18,5 +18,8 @@ public class ValidadorController {
 		}
 		 return "{\"status\": \"invalido\"}";
 	}
-
+	@GetMapping("/")
+	public String index() {
+		return "{\"mensagem\": \"Bem vindo a API valida CPF\"}";
+	}
 }
